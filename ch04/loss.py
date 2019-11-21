5d0dfa6116e05886191edb9eecdfdaa81af50f2b
Sungchan Yi <calofmijuck@snu.ac.kr>
2019-11-22 08:08:21 +0900

    minibatch processing

--- ch04/loss.py
+++ ch04/loss.py
@@ -5,7 +5,16 @@ def mean_squared(x, t):
     return 0.5 * np.sum((x - t) ** 2)
 
 
-def cross_entropy(x, t):
-    ep = 1e-7
-    return -np.sum(t * np.log(x + ep))
+# def cross_entropy(x, t):
+#     ep = 1e-7
+#     return -np.sum(t * np.log(x + ep))
 
+
+def cross_entropy(x, t):
+    if x.ndim == 1:
+        t = t.reshape(1, t.size)
+        x = x.reshape(1, x.size)
+
+    ep = 1e-7
+    batch_size = x.shape[0]
+    return -np.sum(np.log(x[np.arange(batch_size), t] + ep)) / batch_size
