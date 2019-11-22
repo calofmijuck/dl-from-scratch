ce85fe70610fb1c54b32d3c82678eccb8e50faf1
Sungchan Yi <calofmijuck@snu.ac.kr>
2019-11-22 09:32:37 +0900

    Extracted dataset to another directory

--- ch03/predict.py
+++ ch03/predict.py
@@ -1,7 +1,10 @@
+import sys, os
+
+sys.path.append(os.pardir)
 import numpy as np
 import pickle
 from PIL import Image
-from mnist import load_mnist
+from dataset.mnist import load_mnist
 from activation import softmax
 from forward import forward
 
--- ch03/show_mnist.py
+++ ch03/show_mnist.py
@@ -2,7 +2,7 @@ import sys, os
 
 sys.path.append(os.pardir)
 import numpy as np
-from mnist import load_mnist
+from dataset.mnist import load_mnist
 from PIL import Image
 
 
