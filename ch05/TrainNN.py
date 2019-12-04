b9c3d05c2357ca03c2c4c8437feec63d4b9e2dcb
Sungchan Yi <calofmijuck@snu.ac.kr>
2019-12-04 09:24:34 +0900

    Training NN with backprop

--- ch05/TrainNN.py
+++ ch05/TrainNN.py
@@ -4,28 +4,27 @@ sys.path.append(os.pardir)
 import numpy as np
 import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
-from TwoLayer import TwoLayerNet
+from TwoLayerNet import TwoLayerNet
 
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
-train_loss_list = []
-train_acc_list = []
-test_acc_list = []
+network = TwoLayerNet(784, 50, 10)
 
-# Hyperparameters
-iters = 1000
+iters = 10000
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
 iter_epoch = max(train_size // batch_size, 1)
 
-network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
 
 for i in range(iters):
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
 
-    grad = network.numerical_gradient(x_batch, t_batch)
+    grad = network.gradient(x_batch, t_batch)
 
     for key in ("W1", "b1", "W2", "b2"):
         network.params[key] -= learning_rate * grad[key]
@@ -38,8 +37,7 @@ for i in range(iters):
         test_acc = network.accuracy(x_test, t_test)
         train_acc_list.append(train_acc)
         test_acc_list.append(test_acc)
-        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+        print(str(i) + ", train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
 
-plt.plot(range(iters), train_loss_list)
+plt.plot(range(iters)[::10], train_loss_list[::10])
 plt.savefig("fig.png")
-
