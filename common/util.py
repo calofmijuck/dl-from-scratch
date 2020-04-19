77517d35682de57351f9effee347ec7b99f4a438
Sungchan Yi <calofmijuck@snu.ac.kr>
2020-04-19 18:11:56 +0900

    col2im implementation

--- common/util.py
+++ common/util.py
@@ -17,6 +17,11 @@ def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
     # input_data (num, channels, height, width)
 
     N, C, H, W = input_data.shape
+
+    # Should be an integer
+    assert (H + 2 * pad - filter_h) % stride == 0
+    assert (W + 2 * pad - filter_w) % stride == 0
+
     out_h = (H + 2 * pad - filter_h) // stride + 1
     out_w = (W + 2 * pad - filter_w) // stride + 1
 
@@ -61,3 +66,43 @@ def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
     # 2, 3: filter_h * filter_w
 
     return col
+
+
+def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
+    # Inverse of im2col
+    # Transforms 2d array to 4d input
+
+    N, C, H, W = input_shape
+
+    # Should be an integer
+    assert (H + 2 * pad - filter_h) % stride == 0
+    assert (W + 2 * pad - filter_w) % stride == 0
+
+    out_h = (H + 2 * pad - filter_h) // stride + 1
+    out_w = (W + 2 * pad - filter_w) // stride + 1
+
+    # Refer to the comments on im2col
+    # `col` variable was a 6d-array before reshaping, with dimension
+    # (N * out_h * out_w, C * filter_h * filter_w)
+    col = col.reshape(N, out_h, out_w, C, filter_h, filter_w)
+
+    # Now we want to transform this back to the original input, which was
+    # col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
+    # as written in the implementation of im2col
+    # Thus the transpose axis order should be (0, 3, 4, 5, 1, 2)
+    col = col.transpose(0, 3, 4, 5, 1, 2)
+
+    # New array to store the image
+    # In case of index out of range error, add stride - 1 to
+    # 2nd, 3rd dimensions of img
+    img = np.zeros((N, C, H + 2 * pad, W + 2 * pad))
+
+    # Do the exact opposite of im2col
+    for y in range(filter_h):
+        y_max = y + stride * out_h
+        for x in range(filter_w):
+            x_max = x + stride * out_w
+            img[:, :, y:y_max:stride, x:x_max:stride] = col[:, :, y, x, :, :]
+
+    # ignore padded values
+    return img[:, :, pad : H + pad, pad : W + pad]
